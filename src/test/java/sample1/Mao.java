3b109087d17926345130a55763a78a7ae375cfc6
Satabdi28 <chatterjee.shatabdi280@gmail.com>
2020-02-22 11:01:28 +0530

    Hello

--- src/test/java/sample1/Mao.java
+++ src/test/java/sample1/Mao.java
@@ -2,6 +2,7 @@ package sample1;
 
 public class Mao {
 public static void main(String[] args) {
+	System.out.println("Hello!");
 	System.out.println("Update changes");
 	System.out.println("hello");
 }
